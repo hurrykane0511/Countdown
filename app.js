dfcf559649b0f3f40d2dc306ab41299703d8ca02
hurrykane0511 <86058971+hurrykane0511@users.noreply.github.com>
2022-01-01 20:02:41 +0700

    commit

---
+++ app.js
@@ -0,0 +1,54 @@
+"use strict";
+
+//Lấy năm mới
+function getNewYear() {
+  let today = new Date();
+  if (today.getMonth() < 1) {
+    return today.getFullYear();
+  } else {
+    return today.getFullYear() + 1;
+  }
+}
+
+const lunarNewYearDate = new Date(getNewYear(), 1, 1, 0, 0, 0, 0);
+
+function newYear() {
+  const lunarDate = new Date();
+
+  /*Lấy thời gian ngày hiện tại (mily giây) */
+  var ngayHienTai = lunarDate.getTime();
+  var tetAmLich = lunarNewYearDate.getTime();
+
+  /*Tính thời gian còn lại (mily giây) */
+  let thoigianConLai = tetAmLich - ngayHienTai;
+
+  /*Chuyển đơn vị thời gian tương ứng sang mili giây*/
+  var giay = 1000;
+  var phut = giay * 60;
+  var gio = phut * 60;
+  var ngay = gio * 24;
+
+  /*Tìm ra thời gian theo ngày, giờ, phút giây còn lại thông qua cách chia lấy dư(%) và làm tròn số(Math.floor) trong Javascript*/
+  var d = Math.floor(thoigianConLai / ngay);
+  var h = Math.floor((thoigianConLai % ngay) / gio);
+  var m = Math.floor((thoigianConLai % gio) / phut);
+  var s = Math.floor((thoigianConLai % phut) / giay);
+
+  /*Hiển thị kết quả ra các thẻ Div với ID tương ứng*/
+  document.getElementById("days").innerText = d;
+  document.getElementById("hours").innerText = h;
+  document.getElementById("mins").innerText = m;
+  document.getElementById("secs").innerText = s;
+}
+
+/*Thiết Lập hàm sẽ tự động chạy lại sau 1s*/
+setInterval(newYear, 1000);
+
+function StartFireworks() {
+
+  const day = document.querySelector("#days");
+  const hour = document.querySelector("#hours");
+  const min = document.querySelector("#mins");
+  const sec = document.querySelector("#secs");
+
+}
---
+++ firework.js
@@ -0,0 +1,147 @@
+"use strict"
+
+function getRandomInt(min, max) {
+min = Math.ceil(min)
+max = Math.floor(max)
+return Math.floor(Math.random() * (max - min + 1) + min) //The maximum is inclusive and the minimum is inclusive
+}
+
+let canvas = document.getElementById("canvas")
+let ctx = canvas.getContext("2d")
+
+let grd = ctx.createRadialGradient(window.innerWidth/ 2, window.innerHeight/ 2, 200, window.innerWidth/ 2, window.innerHeight/ 2, 600)
+    grd.addColorStop(0, "#62287038")
+    grd.addColorStop(1, "#01001a3d")
+  
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+
+let gravity = -0.1
+
+let fireworks = []
+let subFireworks = []
+
+  
+class Firework {
+    constructor(x,y, radius, velocityX, velocityY, color)
+    {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.velocityX = velocityX
+        this.velocityY = velocityY
+        this.color = color
+        this.opacity = 1
+    }
+
+    update()
+    {
+        this.velocityY -= gravity
+        this.x += this.velocityX
+        this.y += this.velocityY
+        this.opacity -= 0.006
+        if(this.opactiy < 0) this.opacity = 0
+
+    }
+
+    draw()
+    {
+        ctx.save()
+        ctx.shadowBlur = 7
+        ctx.shadowColor = this.color
+        ctx.globalAlpha = this.opacity
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false)
+        ctx.fillStyle = this.color
+        ctx.fill()
+        ctx.closePath()
+        ctx.restore()
+
+    }
+
+}
+
+
+let animate = () => {
+    requestAnimationFrame(animate)
+    update()
+    draw()
+}
+
+let colors = ["Blue", "Orange", "Red", "Purple", "Green"]
+let initializeCount = 0
+let maximumInitialize = 1
+
+let initDelay = 500// ms
+let fireworkRadius = 5
+let particleCount = 50
+let speedMultiplier = 10
+
+let createSubFireworks = (x,y,count,color, speedMultiplier) => {
+  
+    let created = 0
+    let radians = (Math.PI * 2) / count
+
+    while(created < count )
+    {
+        let firework = new Firework(x,y,fireworkRadius,
+                    Math.cos(radians * created) * Math.random() * speedMultiplier,
+                    Math.sin(radians * created) * Math.random() * speedMultiplier, 
+                    colors[Math.floor(Math.random() * colors.length)])
+
+        subFireworks.push(firework)
+  
+        created++
+    }
+
+}
+let update = () => {
+    ctx.fillStyle = grd // this will give tail effect
+    ctx.fillRect(0,0,canvas.width, canvas.height)
+    if(initializeCount < maximumInitialize)
+    {
+        let firework = new Firework(Math.random() * canvas.width,
+                                    canvas.height + Math.random() * 70
+                                    , fireworkRadius,
+                                    3 * (Math.random() - 0.5), -12,
+                                colors[Math.floor(Math.random() * colors.length)])
+        fireworks.push(firework)
+    
+        setTimeout(()=>{
+            initializeCount --
+        }, initDelay)
+        initializeCount ++
+    }
+    fireworks.forEach((firework,i)=>{
+        if(firework.opacity <= 0.1)
+        {
+            fireworks.splice(i,1)
+            createSubFireworks(firework.x, firework.y, particleCount,
+            firework.color, speedMultiplier)
+        }
+        else {
+            firework.update()
+        }
+    })
+    subFireworks.forEach((firework,i)=>{
+        if(firework.opacity <= 0)
+        {
+            subFireworks.splice(i,1)
+        }
+        else {
+            firework.update()
+        }
+    })
+}
+
+
+let draw = () => {
+    fireworks.forEach(firework=> {
+        firework.draw()
+    })
+    subFireworks.forEach(firework=>{
+        firework.draw()
+    })
+}
+    
+  animate()
